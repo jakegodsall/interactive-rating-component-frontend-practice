405173e32df8190c5b42ab5423b3bc1d4c9876eb
jakegodsall <jake.edward.godsall@gmail.com>
2022-08-19 15:12:49 +0100

    completed component

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-const buttonBar = document.querySelector('.options');
-const submitButton = document.querySelector('.submit');
+// two cards
+const ratingCard = document.querySelector('.rating-container');
+const thankYouCard = document.querySelector('.thank-you-container');
+thankYouCard.style.display = "none";
 
-buttonBar.addEventListener('click', e => {
+
+// elements with event listeners
+const buttonBar = document.querySelector('.options');
+const submitButton = document.getElementById('submit');
+
+
+function makeActiveButton(e) {
+    // add class "active" to a button when selected (and remove all others)
     if (e.target.classList.contains('btn')) {
         for (let i = 0; i < buttonBar.children.length; i++) {
             buttonBar.children[i].classList.remove('active');
         }
         e.target.classList.add('active');
         console.log(buttonBar);
-    }
-    
-});
 
-const value = submitButton.addEventListener('click', () => {
-    for (let i = 0; i < buttonBar.children.length; i++) {
-        if (buttonBar.children[i].classList.contains('active')) {
-            return buttonBar.children[i].textConten;
-        }
+        e.preventDefault();
     }
-})
 
+}
+
+function showThankYou(e) {
+    // switch from the ratingCard to the thankYouCard when clicking submit
+    ratingCard.style.display = "none";
+    thankYouCard.style.display = "block";
+
+    const rating = document.querySelector('.active').innerHTML;
+
+    const ratingComp = document.getElementById('rating');
+    ratingComp.innerText = `You selected ${rating} out of 5`
+
+    e.preventDefault();
+}
+
+
+buttonBar.addEventListener('click', makeActiveButton)
+submitButton.addEventListener('click', showThankYou);
